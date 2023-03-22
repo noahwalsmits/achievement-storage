b48cba2639c04b90548db12961db8ca179168e24
noah <noahwalsmits@telfort.nl>
2023-03-22 22:37:38 +0100

    server can request achievements from the database

--- server.js
+++ server.js
@@ -1,36 +1,27 @@
 require('dotenv').config();
+const url = require('url');
 
-var http = require('http');
+const http = require('http');
 const PORT = process.env.PORT || 3000;
 
-var mysql = require('mysql2');
-var database = mysql.createConnection({
+const mysql = require('mysql2');
+const database = mysql.createConnection({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD, //TODO add a password to the database
+    password: process.env.MYSQLPASSWORD,
     port: process.env.MYSQLPORT,
     database: process.env.MYSQLDATABASE
 });
 
 database.connect((err) => {
-    if(err) {
+    if (err) {
         throw err;
     }
     console.log('connected to database!');
 });
 
-function queryDatabase(query) {
-    console.log(query);
-    database.query(query, (err, result) => {
-        if(err) {
-            throw err;
-        }
-        data = result;
-    });
-}
-
 //this is called every time a http request to the server is made
-function onRequest(request, response){
+function onRequest(request, response) {
     response.setHeader('Access-Control-Allow-Origin', '*'); //this allows other sites to access the API
 
     //error handling
@@ -43,8 +34,11 @@ function onRequest(request, response){
         console.error(err);
     });
 
+    let requestPath = url.parse(request.url).pathname;
+    let requestParameters = url.parse(request.url, true).query;
+
     //echo back post messages
-    if(request.method === 'POST' && request.url === '/echo') { 
+    if (requestPath === '/echo' && request.method === 'POST') {
         let body = [];
         request.on('data', (chunk) => {
             body.push(chunk);
@@ -52,18 +46,35 @@ function onRequest(request, response){
             body = Buffer.concat(body).toString();
             response.end(body);
         });
-    } else if(request.method === 'GET') {
-        database.query("SELECT * FROM `DebugTable`", (err, result, fields) => {
-            if(err) {
-                throw err;
-            }
-            response.end(JSON.stringify(result));
-        });
+    } else if (requestPath === '/achievementsByGame' && request.method === 'GET') {
+        handleAchievementsByGameRequest(requestParameters.appId, response);
     } else {
-        response.statusCode = 404; //TODO add the other response codes
+        //request could not be handled
+        response.statusCode = 400;
         response.end();
     }
 }
 
+function handleAchievementsByGameRequest(appId, response) {
+    console.log('requested achievements for appId %d', appId);
+    database.query(
+        'SELECT displayName, description, iconUrl, unlockPercentage FROM Achievement WHERE gameAppId = ' + appId,
+        (err, result) => {
+            if (err) {
+                console.warn(err);
+                response.statusCode = 400;
+                response.end();
+                return;
+            }
+            //if empty array fill database using steam api and run this function again
+            response.end(JSON.stringify(result));
+        }
+    );
+}
+
+function requestGameAchievements(appId) {
+    //request achievements using steam api and put them in the database
+}
+
 http.createServer(onRequest).listen(PORT);
-console.log('server listening to port ${PORT}');
+console.log('server listening to port %d', PORT);
